9918b3dab68778d81bd69e0d1109ab9dfaa1e0b3
boriS <dolbobob00@gmail.com>
2026-05-09 14:24:04 +0300

    design: dark plague & gold animated background

--- lib/core/theme/animated_gradient_background.dart
+++ lib/core/theme/animated_gradient_background.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 class AnimatedGradientBackground extends StatefulWidget {
   final Widget child;
-
   const AnimatedGradientBackground({super.key, required this.child});
 
   @override
@@ -20,7 +19,7 @@ class _AnimatedGradientBackgroundState extends State<AnimatedGradientBackground>
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 12),
+      duration: const Duration(seconds: 20),
     )..repeat(reverse: true);
   }
 
@@ -30,50 +29,29 @@ class _AnimatedGradientBackgroundState extends State<AnimatedGradientBackground>
     super.dispose();
   }
 
-  LinearGradient _buildLightGradient(double t) {
-    final drift = math.sin(t * math.pi * 2) * 0.18;
+  LinearGradient _buildGradient(double t) {
+    final drift = math.sin(t * math.pi * 2) * 0.12;
     return LinearGradient(
-      begin: Alignment(-1.0 + drift, -1.0),
-      end: Alignment(1.0 - drift, 1.0),
+      begin: Alignment(-1.0, -0.8 + drift),
+      end: Alignment(1.0, 0.8 - drift),
       colors: const [
-        Color(0xFFF6F7F4),
-        Color(0xFFEAF4EF),
-        Color(0xFFDCEBE6),
-        Color(0xFFF9EFE6),
+        Color(0xFF080604),
+        Color(0xFF0F0C08),
+        Color(0xFF14100A),
+        Color(0xFF080604),
       ],
-      stops: [0.0, 0.35 + drift * 0.2, 0.72 + drift * 0.08, 1.0],
-    );
-  }
-
-  LinearGradient _buildDarkGradient(double t) {
-    final drift = math.cos(t * math.pi * 2) * 0.15;
-    return LinearGradient(
-      begin: Alignment(-1.0, -0.9 + drift),
-      end: Alignment(1.0, 0.9 - drift),
-      colors: const [
-        Color(0xFF0B0F0D),
-        Color(0xFF101412),
-        Color(0xFF18231F),
-        Color(0xFF0B0F0D),
-      ],
-      stops: [0.0, 0.4 + drift * 0.1, 0.7 + drift * 0.05, 1.0],
+      stops: [0.0, 0.35 + drift * 0.1, 0.70 + drift * 0.05, 1.0],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
     return AnimatedBuilder(
       animation: _controller,
-      builder: (context, _) {
-        final gradient = isDark
-            ? _buildDarkGradient(_controller.value)
-            : _buildLightGradient(_controller.value);
-        return DecoratedBox(
-          decoration: BoxDecoration(gradient: gradient),
-          child: widget.child,
-        );
-      },
+      builder: (context, _) => DecoratedBox(
+        decoration: BoxDecoration(gradient: _buildGradient(_controller.value)),
+        child: widget.child,
+      ),
     );
   }
 }
